ccae2a23f2f4e9b2f72ae64493e9435eab92976e
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2026-06-15 15:52:45 +0800

    优化 WarningCard

--- app/src/main/java/yangfentuozi/dsusideloaderplus/ui/cards/warnings/WarningCard.kt
+++ app/src/main/java/yangfentuozi/dsusideloaderplus/ui/cards/warnings/WarningCard.kt
@@ -3,13 +3,10 @@ package yangfentuozi.dsusideloaderplus.ui.cards.warnings
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import yangfentuozi.dsusideloaderplus.ui.components.SettingsItem
 import yangfentuozi.dsusideloaderplus.ui.theme.AppShape
 
@@ -28,20 +25,10 @@ internal fun WarningCard(
         Column(modifier = Modifier.fillMaxWidth()) {
             SettingsItem(
                 title = title,
+                summary = text,
                 onClick = null,
+                columnTrailingContent = content
             )
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(16.dp)
-            ) {
-                Text(
-                    text = text,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurfaceVariant,
-                )
-                content()
-            }
         }
     }
 }
